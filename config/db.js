b7b387c34596c5b43bce0e4718c5887ab8b290ef
Joshag <tech.gems.biz@gmail.com>
2023-12-07 10:49:31 +0100

    fix db connections

--- config/db.js
+++ config/db.js
@@ -1,30 +1,17 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
 
 const connString =
   process.env.NODE_ENV === "production"
     ? process.env.MONGO_URI
     : process.env.MONGO_LOCAL;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(connString, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
 const dbConn = async () => {
-  let conn;
-
   try {
-    //connect to mongodb
-    // conn = await mongoose.connect(connString);
-
     // Connect the client to the server	(optional starting in v4.7)
-    conn = await client.connect();
+    // await client.connect();
+    await mongoose.connect(connString);
 
-    console.log("Connection to DB succeeded...->", conn.s.url);
+    console.log("Connection to DB succeeded...");
   } catch (err) {
     console.log("Mongo Error: ", err.message);
   }
